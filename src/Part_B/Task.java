207bd047edea46f48c13e5bbf4bd521f76cfad2e
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 <royx520@gmail.com>
2023-01-09 21:23:23 +0200

    update

--- src/Part_B/Task.java
+++ src/Part_B/Task.java
@@ -1,4 +1,63 @@
 package Part_B;
 
-public class Task {
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+public class Task<T> extends FutureTask<T> implements Comparable<Task<T>> {
+
+    private final int priority;
+
+    public Task(Callable<T> operation, int priority) {
+        super(operation);
+        this.priority = priority;
+    }
+
+    public Task(Callable<T> operation) {
+        this(operation, 3);
+    }
+
+    public Task(Callable<T> operation, TaskType type) {
+        this(operation, type.getPriorityValue());
+    }
+
+    public static Task createTask(Callable operation, TaskType type) {
+        return new Task(operation, type);
+    }
+
+    public static Task createTask(Callable operation) {
+        return new Task(operation);
+    }
+
+    public int getPriority() {
+        return this.priority;
+    }
+
+    /**
+     * compare between 2 priority of task
+     *
+     * @param o task to compare
+     * @return
+     */
+    @Override
+    public int compareTo(Task o) {
+        return this.priority - o.getPriority();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(priority);
+    }
+
+    @Override
+    public String toString() {
+        return "Task priority = " + priority + ", is done = " +super.isDone();
+    }
 }
